b65e329fc2a9c8502ef3fc8e0aedf7598ebec4fd
Gabby and Nick Zagorski <nicholas.chug@gmail.com>
2020-03-02 18:34:42 -0500

    added axios and made the github api call

---
+++ src/components/layouts/Spinner.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import Spinner from './spinner.gif';
+
+export const spinner = () => {
+    return <Fragment>
+        <img src={Spinner} alt="Loading..." style={{width:"200px", margin:"auto", display:"block"}} />
+    </Fragment>
+}
+
+export default Spinner;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,40 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layouts/Spinner';
 
-class Users extends Component {
+const Users = ({ users, loading }) => {
 
-    state = {
-        users: [
-            {
-                login: "mojombo",
-                id: 1,
-                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo",
-            },
-            {
-                login: "defunkt",
-                id: 2,
-                avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-                html_url: "https://github.com/defunkt",
-            },
-            {
-                login: "pjhyett",
-                id: 3,
-                avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-                html_url: "https://github.com/pjhyett",
-            },
-        ],
-
-    }
-
-    render() {
+    if (loading) {
+        return <Spinner />
+    } else {
         return (
-            <div style={userStyle}>
-                {this.state.users.map(user => {
-                    return <UserItem key={user.id} user={user} />
-                })}
-            </div>
-        )
+          <div style={userStyle}>
+            {users.map(user => {
+              return <UserItem key={user.id} user={user} />;
+            })}
+          </div>
+        );
     }
 }
 
